8425e0daf6be16ac3aa999e9b227e2e8c63f3efe
Chan Yew Yen <chan.yewyen@gmail.com>
2022-02-11 16:32:49 +0800

    Add rest controllers

--- server/src/main/java/ibf2021/assessment/csf/server/controllers/RecipeRestController.java
+++ server/src/main/java/ibf2021/assessment/csf/server/controllers/RecipeRestController.java
@@ -1,7 +1,13 @@
 package ibf2021.assessment.csf.server.controllers;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URI;
 import java.util.Optional;
 import java.util.logging.Logger;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,21 +15,22 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import ibf2021.assessment.csf.server.models.Recipe;
 import ibf2021.assessment.csf.server.services.RecipeService;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 
 /* Write your request hander in this file */
 
 @RestController
-@RequestMapping(
-    path="/api/recipe",
-    produces=MediaType.APPLICATION_JSON_VALUE
-)
+@RequestMapping(path = "/api/recipe", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RecipeRestController {
     private final Logger logger = Logger.getLogger(RecipeRestController.class.getName());
 
@@ -52,6 +59,43 @@ public class RecipeRestController {
                 .add("message", "Recipe with id %s not found".formatted(recipeId))
                 .build();
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFound.toString());
+        }
+    }
+
+    @PostMapping(
+        consumes=MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> postAddRecipe(@RequestBody String payload) {
+        logger.info("Payload:" + payload);
+
+
+        try {
+            Recipe recipe = new Recipe();
+            try (Reader reader = new StringReader(payload);
+                JsonReader jsonReader = Json.createReader(reader);) {
+
+                JsonObject recipeJson = jsonReader.readObject();
+                recipe.setTitle(recipeJson.getString("title"));
+                recipe.setInstruction(recipeJson.getString("instruction"));
+                recipe.setImage(recipeJson.getString("image"));
+                recipeJson.getJsonArray("ingredients").stream()
+                    .forEach(ingredient -> recipe.addIngredient(ingredient.toString()));
+            } catch (JsonProcessingException jpe) {
+                logger.severe(jpe.getMessage());
+            } catch (IOException ioe) {
+                logger.severe(ioe.getMessage());
+            }
+            logger.info("Adding %s".formatted(recipe));
+            this.recipeService.addRecipe(recipe);
+            URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(recipe.getId()).toUri();
+            return ResponseEntity.created(location).build();
+        } catch (Exception e) {
+            JsonObject errBody = Json.createObjectBuilder()
+                .add("error", e.getMessage())
+                .build();
+            return ResponseEntity.badRequest().body(errBody.toString());
         }
     }
 }
--- server/src/main/java/ibf2021/assessment/csf/server/controllers/RecipesRestController.java
+++ server/src/main/java/ibf2021/assessment/csf/server/controllers/RecipesRestController.java
@@ -1,4 +1,45 @@
 package ibf2021.assessment.csf.server.controllers;
 
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ibf2021.assessment.csf.server.models.Recipe;
+import ibf2021.assessment.csf.server.services.RecipeService;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+
 /* Write your request hander in this file */
 
+@RestController
+@RequestMapping(path = "/api/recipes", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RecipesRestController {
+    private final Logger logger = Logger.getLogger(RecipesRestController.class.getName());
+
+    @Autowired
+    RecipeService recipeService;
+
+    @GetMapping
+    public ResponseEntity<String> getRecipes() {
+        List<JsonObject> recipes = recipeService.getAllRecipes().stream()
+                .map(Recipe.class::cast)
+                .map(item -> Json.createObjectBuilder()
+                        .add("id", item.getId())
+                        .add("name", item.getTitle())
+                        .build()
+                )
+                .toList();
+        // logger.info(recipes.toString());
+        JsonArray recipesJson = Json.createArrayBuilder(recipes).build();
+        logger.info(recipesJson.toString());
+        return ResponseEntity.ok(recipesJson.toString());
+    }
+
+}
